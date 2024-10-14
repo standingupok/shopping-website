4b55092df798ea22509d99ded77c101c3d0cb069
Hoang Long <nguyenhoanglong0165@gmail.com>
2024-10-14 21:10:58 +0700

    Add new model User

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -25,17 +27,38 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constrains: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((res) => {
-    // console.log(res);
-    app.listen(3000);
+    User.findByPk(1);
   })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "long", email: "nguyenhoanglong" });
+    }
+    return user;
+  })
+  .then((user) => app.listen(3000))
   .catch((err) => {
     console.log(err);
   });
